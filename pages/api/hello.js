a8c6e25f90d85c68a08061a0e66b0dd38d53a464
Esi're <esiri92@gmail.com>
2021-05-17 12:52:57 +0100

    Update hello api with name query.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-  res.status(200).json({ text: 'Hello World!' });
+  const { name = 'World' } = req.query;
+  res.status(200).json({ text: `Hello ${name}!` });
 }
