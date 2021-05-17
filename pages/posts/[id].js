da5e47caf27a945db56e4b2d15c8bfc3853d14bc
Esi're <esiri92@gmail.com>
2021-05-17 11:20:45 +0100

    Update Post page with post content.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -8,6 +8,8 @@ const Post = ({ postData }) => (
     {postData.id}
     <br />
     {postData.date}
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>
 );
 
