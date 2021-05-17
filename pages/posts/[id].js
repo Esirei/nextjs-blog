9d2f7ca2239fa2cf6f620aa10b2a9f95ec803f0b
Esi're <esiri92@gmail.com>
2021-05-17 11:04:30 +0100

    Add getStaticPaths to Post page.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,13 @@
 import Layout from '../../components/layout';
+import { getAllPostIds } from '../../lib/posts';
 
 const Post = () => (
   <Layout></Layout>
 );
 
 export default Post;
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return { paths, fallback: false };
+}
